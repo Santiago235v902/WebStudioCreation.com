70bdb7bcc93b5d5fa12aedb921a550f5942a9ead
PaginasWebs <153400127+Santiago235v902@users.noreply.github.com>
2024-02-08 22:16:16 -0300

    Add files via upload

---
+++ modulo3.js
@@ -0,0 +1,17 @@
+function redireccionar(url) {
+    window.location.href = url;
+}
+// Definimos la frase secreta
+const fraseSecreta = "secreto";
+
+// Añadimos un evento de teclado al documento
+document.addEventListener("keyup", function(event) {
+  // Obtenemos el texto que ha escrito el usuario
+  const textoUsuario = event.target.value.toLowerCase();
+
+  // Verificamos si el texto del usuario coincide con la frase secreta
+  if (textoUsuario.includes(fraseSecreta)) {
+    // Si coincide, redirigimos a la página deseada
+    window.location.href = "nuevomodulo.html";
+  }
+}); 
