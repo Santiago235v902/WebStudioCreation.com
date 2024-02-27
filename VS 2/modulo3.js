9605c7405bce9ada51e26834665d6719544791b1
PaginasWebs <153400127+Santiago235v902@users.noreply.github.com>
2024-02-27 15:47:33 -0300

    Add files via upload

---
+++ VS 2/modulo3.js
@@ -0,0 +1,30 @@
+function redireccionar(url) {
+    window.location.href = url;
+}// Definimos la frase secreta
+const fraseSecreta = "secreto";
+
+// Añadimos un evento de teclado al documento
+document.addEventListener("keyup", function(event) {
+  // Obtenemos el texto que ha escrito el usuario
+  const textoUsuario = event.target.value.toLowerCase();
+
+  // Verificamos si el texto del usuario coincide con la frase secreta
+  if (textoUsuario.includes(fraseSecreta)) {
+    // Si coincide, redirigimos a la página deseada
+    window.location.href = "nuevomodulo.html";
+  }
+});
+
+const fraseGhost = "ghost";
+
+// Añadimos un evento de teclado al documento
+document.addEventListener("keyup", function(event) {
+  // Obtenemos el texto que ha escrito el usuario
+  const textoUsuario = event.target.value.toLowerCase();
+
+  // Verificamos si el texto del usuario coincide con la frase secreta
+  if (textoUsuario.includes(fraseGhost)) {
+    // Si coincide, redirigimos a la página deseada
+    window.location.href = "https://leobay23.github.io/index.html";
+  }
+});
